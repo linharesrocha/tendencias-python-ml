922edc3554b08fa2f695dbe5c24e80dd07b80891
linharesrocha <guilhermelinharesr@gmail.com>
2022-10-08 13:44:13 -0300

    linux install

--- install-linux.sh
+++ install-linux.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 cd ~
 mkdir workspace
+cd workspace
+git clone https://github.com/linharesrocha/tendencias-python-ml.git
+cd tendencias-python-ml/
+sudo apt update -y
+sudo apt install python3-pip -y
+pip install -r requirements.txt
+pip install XlsxWriter
+pip install slackclient
+cd ~
+rm install-linux.sh
 
